58749287862845705a268dea834a9ba7deccacb4
admin <saidsadyk2907@gmail.com>
2024-10-02 15:43:48 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -17,46 +17,46 @@ public static void String (String name){
 
 
 
-//    public static int[] MyMassive(int a){
-//        Random random=new Random();
-//        int[]b=new int[a];
-//        for (int i =0;i<a;i++){
-//            int s = b[i]=random.nextInt(1,10);
-//            System.out.print(s*2+"," );
-//        }
-//        System.out.println(" ");
-//        return b;
-//    }
-//    public static void myVoid1(){
-//        int [] taksan = new int[100];
-//        int a =1;
-//        for (int i =0;i< taksan.length;i++){
-//            taksan [i]=a;
-//            a=a+2;
-//        }
-//        System.out.println(Arrays.toString(taksan));
-//        for (int b =0;b< taksan.length;b++){
-//            int j=0;
-//            a=a-2;
-//            System.out.print(a+", ");
-//        }}
-//public static void myVoid2(){
-//        Random random = new Random();
-//        int[] nums1 = new int[15];
-//        int b = 0;
-//        for (int a = 0; a < nums1.length; a++) {
-//            int i = random.nextInt(1, 10);
-//            nums1[a] = i;
-//            if (i % 2 == 0) {
-//                b++;
-//            }
-//        }
-//        System.out.println(Arrays.toString(nums1));
-//        System.out.println("жуп сан: " + (b));
-//        System.out.println("так сан: " + (nums1.length - b));
-//    }
-//    public static boolean myVoid3(int a,int b){
-//        if (a==b){return true;}
-//        else {return false;}
-//    }
+    public static int[] MyMassive(int a){
+        Random random=new Random();
+        int[]b=new int[a];
+        for (int i =0;i<a;i++){
+            int s = b[i]=random.nextInt(1,10);
+            System.out.print(s*2+"," );
+        }
+        System.out.println(" ");
+        return b;
+    }
+    public static void myVoid1(){
+        int [] taksan = new int[100];
+        int a =1;
+        for (int i =0;i< taksan.length;i++){
+            taksan [i]=a;
+            a=a+2;
+        }
+        System.out.println(Arrays.toString(taksan));
+        for (int b =0;b< taksan.length;b++){
+            int j=0;
+            a=a-2;
+            System.out.print(a+", ");
+        }}
+public static void myVoid2(){
+        Random random = new Random();
+        int[] nums1 = new int[15];
+        int b = 0;
+        for (int a = 0; a < nums1.length; a++) {
+            int i = random.nextInt(1, 10);
+            nums1[a] = i;
+            if (i % 2 == 0) {
+                b++;
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
+        System.out.println("жуп сан: " + (b));
+        System.out.println("так сан: " + (nums1.length - b));
+    }
+    public static boolean myVoid3(int a,int b){
+        if (a==b){return true;}
+        else {return false;}
+    }
 }
